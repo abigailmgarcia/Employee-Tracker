2f539e37ca1ebb20c0b5d00ef6ef1b8131831d63
abigailmgarcia <abigailmg31@gmail.com>
2023-07-29 08:48:58 -0500

    add switch

--- server.js
+++ server.js
@@ -1,25 +1,62 @@
-const express = require('express')
+const { questions } = require("./inquireFile")
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+require('dotenv').config();
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //connect db
-const db = mysql.createConnection(
+const dbConfig = 
     {
-        host: 'localhost',
-        user:'root',
-        password:'',
-        database:'employee_db'
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
+    };
+
+    async function main(){
+        const connection = await mysql.createConnection(dbConfig);
+        const answers = await inquirer.prompt(questions);
+
+        //handle user input
+        switch(answers.options){
+            case 'View All Departments':
+                const [departments] = await connection.query('SELECT * FROM departments');
+                console.log(departments);
+                break;
+            case 'View All Roles':
+                const [roles] = await connection.query('SELECT * FROM role');
+                console.log(roles);
+                break;
+            case 'View All Employees':
+                cosnt [employees] = await connection.query('SELECT * FROM employee');
+                console.log(employees);
+                break;
+            case 'Add a Department':
+                await connection.query('INSERT INTO department (name) VALUES (?)', [answers.departmentName]);
+                break;
+            case 'Add a Role':
+                await connection.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [answers.roleTitle, answers.roleSalary, answers.roleDepartment]);
+                console.log('Role added succesfully');
+                break;
+            case 'Add an Employee':
+                 await connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [answers.firstName, answers.lastName, answers.role, answers.manager]);
+                 console.log('employee added successfully');
+                 break;
+            case 'Update an Employee':
+                console.log('Update an employee');
+                break;
+            case 'Update an Employee Role':
+                    console.log('Update an Employee Role');
+                break;
+            default:
+                console.log('Invalid option selected.');
+        }
     }
-)
 
-db.connect((err) =>{
-    if (err) throw err;
-    console.log('connected to database');
-});
+    // close db
+    await connection.end();
 
-app.listen(PORT, () =>{
-    console.log(`server running on port ${PORT}`)
-});
+    main();
