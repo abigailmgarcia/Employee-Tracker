7cf500e7eb3f63fb80dd7a62febad76efaf2a883
abigailmgarcia <abigailmg31@gmail.com>
2023-07-30 03:46:21 -0500

    changed department int

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE department(
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    department_id INT NOT NULL,
+    department_id INT,
     salary INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
